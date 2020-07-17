05b285126ec8843adc610bac7486b63fb1bf16db
Dozie Agbai <dozie@vouchrapp.co>
2020-07-17 17:00:12 -0400

    debuging category function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,9 +18,8 @@ export const eventSubscription = functions.https.onRequest((request, response) =
 
 
 
-
 export const templateCommand = functions.https.onRequest((request, response) => {
-    const { command } = request.params;
+    const { command } = request.body;
 
     if (command !== Command.TEMPLATE) {
         response.send();
@@ -33,34 +32,45 @@ function processException(error: Error) {
     if (error.code === ErrorCode.TIMEOUT) {
         object = {
             "response_type": "in_channel",
-            "text": "Unable to process command. try again!"
+            "text": "Unable to process command. try again!",
+            "status": 400
         };
     } else if (error.code === ErrorCode.UNAUTHORIZED) {
         object = {
             "response_type": "in_channel",
-            "text": "Unauthorized request"
+            "text": "Unauthorized request",
+            "status": 401
         };
     }
     return object
 }
 
 
-export const categoryCommand = functions.https.onRequest((request, response) => {
+export const slashCommand = functions.https.onRequest((request, response) => {
     functions.logger.info("request body", request.body);
-    const { command } = request.params;
-
+    const { command } = request.body;
     if (command !== Command.CATEGORY) {
         functions.logger.info("invalid command: ", command);
         response.send();
         return;
     }
     try {
+        functions.logger.info("=============", functions.config().slack.signing.secret);
         validationService.validateRequest(functions.config().slack.signing.secret, request);
+        functions.logger.info("after validation of request");
     } catch (exception) {
-        functions.logger.error(exception.getMessage(), exception);
-        const error = processException(exception);
-        response.sendStatus(400);
-        response.send(error);
+        functions.logger.error(exception);
+        if (exception instanceof Error) {
+            const error = processException(exception);
+            if (error) {
+                response.sendStatus(error.status);
+                delete error.status;
+            }
+            response.send(error)
+        } else {
+            response.send(exception);
+        }
+        response.send(exception);
         return;
     }
 
--- functions/src/service/validation.service.ts
+++ functions/src/service/validation.service.ts
@@ -7,22 +7,25 @@ export class ValidationService {
     readonly version: string = 'v0';
 
     validateRequest(signingSecret: string, request: functions.https.Request): void {
-        let currentTime: number = new Date().getTime();
+        const currentTime: number = new Date().getTime();
         const { 'X-Slack-Request-Timestamp': timestamp } = request.headers;
         const contents: string = [this.version, timestamp, request.body].join(':');
         if (timestamp && parseInt(timestamp.toString())) {
-            let timeStampToMilli = parseInt(timestamp.toString()) * 60 * 5 * 1000;
+            const timeStampToMilli = parseInt(timestamp.toString()) * 60 * 5 * 1000;
             if (currentTime > timeStampToMilli) {
                 throw new TimeoutException("request was made a while ago");
             } else {
-                let secret = crypto.createHmac('sha256', signingSecret)
+                const secret = crypto.createHmac('sha256', signingSecret)
                     .update(contents)
                     .digest('hex');
 
+                console.log('comparing signatures now');
                 if (crypto.timingSafeEqual(Buffer.from(signingSecret), Buffer.from(secret))) {
                     throw new UnauthorizedException("unauthorized request");
                 }
             }
+        } else {
+            throw new UnauthorizedException("unauthorized request");
         }
     }
 }
